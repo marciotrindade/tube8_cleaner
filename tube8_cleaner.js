b6b6d1336c364f9c426b3e546400418c47d21889
Marcio Trindade <marciotrindade@gmail.com>
2013-07-30 09:45:01 -0300

    add tube8_cleaner files

---
+++ tube8_cleaner.js
@@ -0,0 +1,2 @@
+$("iframe, .box-comments, .box-thumbnail-friends, .title-wrapper, .title-bar, .name-ps-trigger, #shareMenu, .banner-container, .bkg-video-rating-box, .buttons-video-col").remove();
+$(".video-col-01").removeClass('float-left').width('auto');
